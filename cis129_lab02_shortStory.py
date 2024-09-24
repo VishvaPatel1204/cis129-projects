4590737398bb0b2e7987eb71a1779ab55f22e472
VishvaPatel1204 <vpatel9@mail.pima.edu>
2024-09-24 12:26:47 -0700

    Update cis129_lab02_shortStory.py
    Deleted Python "Hello World " and added some ones.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,5 @@
-print('Hello, World!')
+#Auther: Vishva Patel
+#My first python lab in my CIS129 course
+#Prints a simple short story 
+print('Vishva patel')
+print('These is my First time using GitHub')
